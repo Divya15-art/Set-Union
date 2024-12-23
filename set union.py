01444aefc3995fb2848c3acd90e6ff7da22f5385
@Divya15-art <chandakdivya14@gmail.com>
2024-12-23 21:40:42 +0530

    first commit

---
+++ set union.py
@@ -0,0 +1,8 @@
+set1={}
+print(set1)
+set1={3,3,4,4,4,5,5,6}
+set2={6,6,7,7,7,8,8,3,4}
+print(set1.union(set2))
+print(set1.intersection(set2))
+print(set1.difference(set2))
+print(set1.symmetric_difference(set2))
